583fbfe8e1fd3445d32129b562177d1d2c5189a4
Henning Dahlheim <hactar@cyberkraft.ch>
2022-12-04 11:50:08 +0100

    2022 Day 4 and Day 5 prep

--- test/aoc2022/d4_test.exs
+++ test/aoc2022/d4_test.exs
@@ -3,7 +3,6 @@ defmodule Aoc.Aoc2022.D4Test do
   alias Aoc.Aoc2022.D4
   doctest Aoc.Aoc2022.D4
 
-  @tag :skip
   test "part 1" do
     res =
       "./test/aoc2022/input/d4.txt"
@@ -11,10 +10,9 @@ defmodule Aoc.Aoc2022.D4Test do
       |> D4.part_1()
       |> IO.inspect(label: :part_1)
 
-    assert _ == res
+    assert 602 == res
   end
 
-  @tag :skip
   test "part 2" do
     res =
       "./test/aoc2022/input/d4.txt"
@@ -22,6 +20,6 @@ defmodule Aoc.Aoc2022.D4Test do
       |> D4.part_2()
       |> IO.inspect(label: :part_2)
 
-    assert _ == res
+    assert 0 == res
   end
 end
