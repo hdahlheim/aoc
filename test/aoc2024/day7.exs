30d6379640e6113a3ac6a84391fd6ba1639e75e1
Henning Dahlheim <dev@dahlheim.ch>
2024-12-08 11:59:33 +0100

    remove skip from day7 test

--- test/aoc2024/day7.exs
+++ test/aoc2024/day7.exs
@@ -3,7 +3,6 @@ defmodule Aoc.Aoc2024.Day7Test do
   alias Aoc.Aoc2024.Day7
   doctest Aoc.Aoc2024.Day7
 
-  @tag :skip
   test "aoc day7 part1" do
     assert 945_512_582_195 ==
              "./test/aoc2024/input/day7.txt"
@@ -12,9 +11,8 @@ defmodule Aoc.Aoc2024.Day7Test do
              |> IO.inspect(label: :part1)
   end
 
-  @tag :skip
   test("aoc day7 part2") do
-    assert 0 ==
+    assert 271_691_107_779_347 ==
              "./test/aoc2024/input/day7.txt"
              |> File.read!()
              |> Day7.part_2()
